34ccade00973cfbfd587e9ef9c4ed12db012e95f
luke <no-email@>
2015-09-11 16:16:41 +0000

    [demo] added bunch of plots

--- app.R
+++ app.R
@@ -2,17 +2,30 @@ library(shiny)
 
 # Global variables can go here
 n <- 200
+m <- 100
 
 # Define the UI
 ui <- bootstrapPage(
   numericInput('n', 'Number of obs', n),
-  plotOutput('plot')
+  numericInput('m', 'Number of obs 2', m),
+  dateInput('myDate', 'Some Date input'),
+  plotOutput('plot'),
+  plotOutput('plot2'),
+  verbatimTextOutput('myTextOutput')
 )
 
 # Define the server code
 server <- function(input, output) {
   output$plot <- renderPlot({
-    hist(runif(input$n))
+    hist(runif(input$n + input$m))
+  })
+
+  output$plot2 <- renderPlot({
+    plot(1:input$m, type='l', col='red')
+  })
+
+  output$myTextOutput <- renderText({
+    as.character(input$myDate)
   })
 }
 
